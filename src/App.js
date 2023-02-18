0a5fa21a6a8b8d59beae837481abe698aa436fdc
c00p75_ <georgecoopmsapenda@gmail.com>
2023-02-18 12:12:16 +0200

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
 
 function App() {
-  const navigate = useNavigate();
-  const back = () => navigate(-1);
   return (
     <div className="App">
       <Routes>
